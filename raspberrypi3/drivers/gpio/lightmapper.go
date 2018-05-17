0abc8902c770ef28517ef5a37a091ff1d91cb2ea
Sanil Kumar <skdwriting@gmail.com>
2018-05-17 11:37:14 +0530

    Added code to update the actual value after changing the state of the light

--- raspberrypi3/drivers/gpio/lightmapper.go
+++ raspberrypi3/drivers/gpio/lightmapper.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"runtime"
 	"DeviceCertification/raspberrypi3/drivers/gpio/devicedrivers/lightdriver"
+	"bytes"
 )
 
 
@@ -23,10 +24,14 @@ type WatchResponse struct {
 	Content []Content
 }
 
+
+
+
 func main()  {
 	fmt.Printf("Start light mapper ARCH [%s]\n", runtime.GOARCH)
 
 	url := "http://localhost:8080/v1.0/p1/edgecloud/edges/e1/ldrs/expected/light?watch=true&recursive=true"
+	targetUrl:="http://localhost:8081/v1.0/HuaweiProject1/edgecloud/edges/e1/ldrs/actual/light"
 	req, _ := http.NewRequest("GET", url, nil)
 	resp, _ := http.DefaultClient.Do(req)
 	defer resp.Body.Close()
@@ -44,22 +49,57 @@ func main()  {
 		for _, c := range w.Content {
 			status := c.Value.(float64)
 			fmt.Println(status)
-
 			if runtime.GOARCH == "amd64" {
 				if status > 0 {
 					fmt.Println("Light turned on")
+					UpdateActual(c, targetUrl)
 				} else {
 					fmt.Println("Light turned off")
+					UpdateActual(c, targetUrl)
 				}
 			} else {
 				if status > 0 {
 					lightdriver.TurnON()
+					UpdateActual(c, targetUrl)
 				} else {
 					lightdriver.TurnOff()
+					UpdateActual(c, targetUrl)
 				}
 			}
+
 		}
 
 	}
+
+}
+
+func UpdateActual(c Content,actualUrl string){
+	actualValueUpdation:=Content{
+		Key:c.Key,
+		Value:c.Value,
+	}
+	body,_:=json.Marshal(actualValueUpdation)
+	fmt.Println("Actual Value updated")
+	PostObj(body,actualUrl)
+}
+
+
+
+func PostObj(b []byte, url string){
+
+	req, err := http.NewRequest("PUT", url, bytes.NewBuffer(b))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("response Status:", resp.Status)
 
 }
